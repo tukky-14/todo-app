b590bf138cb6a7eea4f71f0ecae18a290dbed486
Eiji Otsuki <bigoonage0104@gmail.com>
2020-06-25 19:36:51 +0900

    Create folders

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
